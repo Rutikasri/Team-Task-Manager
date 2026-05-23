85f5bf2a5699854ff4582dacb4f710ada9b054aa
Rutikasri <rutikasripotnuru20@gmail.com>
2026-05-23 16:47:56 +0530

    commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "https://team-task-manager-api-p16q.onrender.com",
+  baseURL: "https://team-task-manager-api-p16q.onrender.com/api",
 })
 
 API.interceptors.request.use((req) => {
